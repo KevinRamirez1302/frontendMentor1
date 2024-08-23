8f201aacf6de3c3c4afc72daba2193ed1d275bfb
kevin <kevin.alexanderra1302@gmail.com>
2024-08-23 08:19:17 -0400

    laptop fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import './App.css';
 import calculator from '/calculador.png';
 
+import Calculator from '/calculador.png';
 function App() {
   return (
     <>
-      <section className=" flex justify-center items-center bg-blue-200 w-full h-screen">
-        <div className=" bg-white flex ">
-          <div className=" flex justify-around items-center w-2/4">
-            <div>
-              <h1>Mortgage calculator</h1>
-              <p className=" underline">clear all</p>
+      <section className=" bg-blue-200 flex justify-center items-center w-full h-screen">
+        <div className=" shadow-xl bg-white flex w-[65%] h-[80%] rounded-2xl">
+          <div className=" w-2/4 h-full ">
+            <div className=" flex my-[32px] mx-[20px] justify-between">
+              <h1 className=" font-bold">Mortgage calculator</h1>
+              <p className=" cursor-pointer text-sm underline">Clear all</p>
             </div>
-            <div></div>
           </div>
-          <div className=" w-2/4 flex flex-col items-center justify-center">
-            <img className=" w-14" src={calculator} alt="" />
-            <div className=" flex-col">
-              <h1>Result show here</h1>
-              <p>
-                Complete the form and click "calculated repayment" to see you
-                what your monthly repayment would be
-              </p>
+          <div>
+            <div>
+              <label htmlFor="">Mortgage calculator</label>
+              <div></div>
             </div>
+          </div>
+          <div className=" w-2/4 bg-gray-800 flex flex-col justify-center items-center px-[2%] py-[16%] h-full rounded-2xl ">
+            <img className=" w-[20%]" src={Calculator} alt="" />
+            <h2 className=" text-white font-bold m-3">Result show here</h2>
+            <p className=" text-center text-gray-400 text-[12px]">
+              Complete de form and click "calculate repayment " to see what your
+              monthly repayment would be
+            </p>
           </div>
         </div>
       </section>
